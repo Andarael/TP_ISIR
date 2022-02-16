15c6f2e5c9664d08dd094cc982f22c2806cca3c7
Andarael <josuraad@gmail.com>
2022-02-16 12:10:30 +0100

    implemented camera rays

--- src/cameras/perspective_camera.cpp
+++ src/cameras/perspective_camera.cpp
@@ -1,28 +1,50 @@
 #include "perspective_camera.hpp"
+
 #include <glm/gtx/string_cast.hpp>
 
 namespace RT_ISICG
 {
-	PerspectiveCamera::PerspectiveCamera( const float p_aspectRatio ) : _aspectRatio( p_aspectRatio )
-	{
-		_updateViewport();
-	}
+    PerspectiveCamera::PerspectiveCamera(const float p_aspectRatio) : _aspectRatio(p_aspectRatio)
+    {
+        _updateViewport();
+    }
 
-	PerspectiveCamera::PerspectiveCamera( const Vec3f & p_position,
-										  const Vec3f & p_lookAt,
-										  const Vec3f & p_up,
-										  const float	p_fovy,
-										  const float	p_aspectRatio )
-		: BaseCamera( p_position ), _fovy( p_fovy ), _aspectRatio( p_aspectRatio )
-	{
-		/// TODO ! _u ? _v ? _w ?
+    PerspectiveCamera::PerspectiveCamera(const Vec3f &p_position,
+                                         const Vec3f &p_lookAt,
+                                         const Vec3f &p_up,
+                                         const float p_fovy,
+                                         const float p_aspectRatio)
+        : BaseCamera(p_position), _fovy(p_fovy), _aspectRatio(p_aspectRatio)
+    {
+        /// TODO ! _u ? _v ? _w ?
+        _updateViewport();
+    }
 
-		_updateViewport();
-	}
+    void PerspectiveCamera::displayCamera()
+    {
+        std::cout << " ===== camera ====== " << std::endl;
+        std::cout << "      position : " << glm::to_string(_position) << std::endl;
+        std::cout << "      center : " << glm::to_string(_center) << std::endl;
+        std::cout << "      _viewportTopLeftCorner : " << glm::to_string(_viewportTopLeftCorner) << std::endl;
+        std::cout << "      _viewportV : " << glm::to_string(_viewportV) << std::endl;
+        std::cout << "      _viewportU : " << glm::to_string(_viewportU) << std::endl;
+        std::cout << " ================== " << std::endl;
+    }
 
-	void PerspectiveCamera::_updateViewport()
-	{
-		/// TODO ! _viewportTopLeftCorner ?	_viewportU ? _viewportV ?
-	}
+    void PerspectiveCamera::_updateViewport()
+    {
+        Vec3f _center = _position + (-_w * _focalDistance);
+
+        float viewPortHeight = glm::cos(glm::radians(_fovy)) * _focalDistance * 2.0f;
+        float viewPortWidth = viewPortHeight * _aspectRatio;
+
+        _viewportV = _v * viewPortHeight;
+        _viewportU = _u * viewPortWidth;
+
+        _viewportTopLeftCorner = _center + _viewportV * 0.5f;
+        _viewportTopLeftCorner = _viewportTopLeftCorner - _viewportU * 0.5f;
+
+        displayCamera();
+    }
 
 } // namespace RT_ISICG
--- src/cameras/perspective_camera.hpp
+++ src/cameras/perspective_camera.hpp
@@ -3,45 +3,54 @@
 
 #include "base_camera.hpp"
 
+#include "glm/gtx/string_cast.hpp"
+
 namespace RT_ISICG
 {
-	class PerspectiveCamera : public BaseCamera
-	{
-	  public:
-		PerspectiveCamera( const float p_aspectRatio );
+    class PerspectiveCamera : public BaseCamera
+    {
+    public:
+        PerspectiveCamera(const float p_aspectRatio);
 
-		PerspectiveCamera( const Vec3f & p_position,
-						   const Vec3f & p_lookAt,
-						   const Vec3f & p_up,
-						   const float	 p_fovy,
-						   const float	 p_aspectRatio );
+        PerspectiveCamera(const Vec3f &p_position,
+                          const Vec3f &p_lookAt,
+                          const Vec3f &p_up,
+                          const float p_fovy,
+                          const float p_aspectRatio);
 
-		~PerspectiveCamera() = default;
+        ~PerspectiveCamera() = default;
 
-		inline Ray generateRay( const float p_sx, const float p_sy ) const override
-		{
-			/// TODO !
-			return Ray( Vec3f( 0.f ), Vec3f( 0.f, 0.f, 1.f ) );
-		}
+        inline Ray generateRay(const float p_sx, const float p_sy) const override
+        {
+            Vec3f origin = _position;
 
-	  private:
-		void _updateViewport();
+            Vec3f direction = _viewportTopLeftCorner + (p_sx * _viewportU) - (p_sy * _viewportV);
+            direction = glm::normalize(direction);
 
-	  private:
-		float _fovy			 = 60.f;
-		float _focalDistance = 1.f;
-		float _aspectRatio	 = 1.f;
+            return Ray(origin, direction);
+        }
 
-		// Local coordinates system
-		Vec3f _u = Vec3f( 1.f, 0.f, 0.f );
-		Vec3f _v = Vec3f( 0.f, 1.f, 0.f );
-		Vec3f _w = Vec3f( 0.f, 0.f, -1.f );
+        void displayCamera();
 
-		// Viewport data
-		Vec3f _viewportTopLeftCorner = VEC3F_ZERO; // Top left corner position
-		Vec3f _viewportU			 = VEC3F_ZERO; // Horizontal vector
-		Vec3f _viewportV			 = VEC3F_ZERO; // Vertical vector
-	};
+    private:
+        void _updateViewport();
+
+    private:
+        float _fovy = 60.f;
+        float _focalDistance = 1.f;
+        float _aspectRatio = 1.f;
+
+        // Local coordinates system
+        Vec3f _u = Vec3f(1.f, 0.f, 0.f);
+        Vec3f _v = Vec3f(0.f, 1.f, 0.f);
+        Vec3f _w = Vec3f(0.f, 0.f, -1.f);
+
+        // Viewport data
+        Vec3f _viewportTopLeftCorner = VEC3F_ZERO; // Top left corner position
+        Vec3f _viewportU = VEC3F_ZERO;             // Horizontal vector
+        Vec3f _viewportV = VEC3F_ZERO;             // Vertical vector
+        Vec3f _center = VEC3F_ZERO;                // Screen center
+    };
 } // namespace RT_ISICG
 
 #endif // __RT_ISICG_PERSPECTIVE_CAMERA__
