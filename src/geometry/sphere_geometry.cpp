0c53dcf6bdd125af1a78e71ff5abb1b8e7fcd730
Andarael <josuraad@gmail.com>
2022-02-16 12:23:06 +0100

    format

--- src/geometry/sphere_geometry.cpp
+++ src/geometry/sphere_geometry.cpp
@@ -2,14 +2,14 @@
 
 namespace RT_ISICG
 {
-	bool SphereGeometry::intersect( const Ray & p_ray, float & p_t1, float & p_t2 ) const
-	{
-		/// TODO !
+    bool SphereGeometry::intersect(const Ray &p_ray, float &p_t1, float &p_t2) const
+    {
+        /// TODO !
 
-		p_t1 = -1.f;
-		p_t2 = -1.f;
+        p_t1 = -1.f;
+        p_t2 = -1.f;
 
-		return false;
-	}
+        return false;
+    }
 
 } // namespace RT_ISICG
--- src/geometry/sphere_geometry.hpp
+++ src/geometry/sphere_geometry.hpp
@@ -5,29 +5,29 @@
 
 namespace RT_ISICG
 {
-	class SphereGeometry : public BaseGeometry
-	{
-	  public:
-		SphereGeometry()		  = delete;
-		virtual ~SphereGeometry() = default;
+    class SphereGeometry : public BaseGeometry
+    {
+    public:
+        SphereGeometry() = delete;
+        virtual ~SphereGeometry() = default;
 
-		SphereGeometry( const Vec3f & p_center, const float p_radius ) : _center( p_center ), _radius( p_radius ) {}
+        SphereGeometry(const Vec3f &p_center, const float p_radius) : _center(p_center), _radius(p_radius) {}
 
-		inline const Vec3f & getCenter() const { return _center; }
-		inline const float	 getRadius() const { return _radius; }
+        inline const Vec3f &getCenter() const { return _center; }
+        inline const float getRadius() const { return _radius; }
 
-		bool intersect( const Ray & p_ray, float & p_t1, float & p_t2 ) const;
+        bool intersect(const Ray &p_ray, float &p_t1, float &p_t2) const;
 
-		inline Vec3f computeNormal( const Vec3f & p_point ) const
-		{
-			/// TODO !
-			return Vec3f( 1.f );
-		}
+        inline Vec3f computeNormal(const Vec3f &p_point) const
+        {
+            /// TODO !
+            return Vec3f(1.f);
+        }
 
-	  private:
-		Vec3f _center = VEC3F_ZERO;
-		float _radius = 1.f;
-	};
+    private:
+        Vec3f _center = VEC3F_ZERO;
+        float _radius = 1.f;
+    };
 
 } // namespace RT_ISICG
 
