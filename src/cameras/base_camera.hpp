f3b2e0c5f573645172b104d83a68e32486ec6c5e
Andarael <josuraad@gmail.com>
2022-02-16 12:09:10 +0100

    reformat

--- src/cameras/base_camera.hpp
+++ src/cameras/base_camera.hpp
@@ -6,20 +6,20 @@
 
 namespace RT_ISICG
 {
-	class BaseCamera
-	{
-	  public:
-		BaseCamera()		  = default;
-		BaseCamera( const Vec3f & p_position ) : _position( p_position ) {}
-		virtual ~BaseCamera() = default;
+    class BaseCamera
+    {
+    public:
+        BaseCamera() = default;
+        BaseCamera(const Vec3f &p_position) : _position(p_position) {}
+        virtual ~BaseCamera() = default;
 
-		const Vec3f & getPosition() const { return _position; }
+        const Vec3f &getPosition() const { return _position; }
 
-		virtual inline Ray generateRay( const float p_sx, const float p_sy ) const = 0;
+        virtual inline Ray generateRay(const float p_sx, const float p_sy) const = 0;
 
-	  protected:
-		Vec3f _position = VEC3F_ZERO;
-	};
+    protected:
+        Vec3f _position = VEC3F_ZERO;
+    };
 
 } // namespace RT_ISICG
 
