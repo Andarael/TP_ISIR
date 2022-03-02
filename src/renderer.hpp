5e2d2ddb2cd6c3ae42a2d836d15c983328470e0c
Andarael <josuraad@gmail.com>
2022-03-02 10:34:00 +0100

    added AA method

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -22,6 +22,20 @@ namespace RT_ISICG
 
         float renderImage(const Scene &p_scene, const BaseCamera *p_camera, Texture &p_texture);
 
+        /**
+         * @brief Render multiple sample per pixel and fill the given color
+         *
+         * @param p_camera
+         * @param sx pixel pos in x
+         * @param sy pixel pos in y
+         * @param color color to fill
+         * @param p_scene scene to render
+         * @param pixelSizeY size of pixel in X
+         * @param pixelSizeX size of pixel in Y
+         * @param nbPixelSamples number of samples
+         */
+        void multiSample(const RT_ISICG::BaseCamera *p_camera, float sx, float sy, RT_ISICG::Vec3f &color, const RT_ISICG::Scene &p_scene, float pixelSizeY, float pixelSizeX, int nbPixelSamples);
+
     private:
         BaseIntegrator *_integrator = nullptr;
         int _nbPixelSamples = 1;
