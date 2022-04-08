658bbe5bee0ea74d88b522e5c34ded121084fc54
Andarael <josuraad@gmail.com>
2022-04-08 03:33:34 +0200

    added additive to shade

--- src/materials/PlasticMaterial.hpp
+++ src/materials/PlasticMaterial.hpp
@@ -12,14 +12,22 @@ namespace RT_ISICG
     class PlasticMaterial : public BaseMaterial
     {
     public:
-        PlasticMaterial(const std::string &p_name, const Vec3f &p_diffuse, const float p_factor, const float p_shininess)
-            : BaseMaterial(p_name), _factor(p_factor), _lambert_brdf(p_diffuse * p_factor), _phong_brdf((1.f - p_factor) * p_diffuse, p_shininess){};
+        PlasticMaterial(const std::string &p_name, const Vec3f &p_color, const float p_metalness, const float p_shininess, const bool p_additive = true)
+            : BaseMaterial(p_name),
+              _additive(p_additive),
+              _metalness(p_metalness),
+              _lambert_brdf(p_color),
+              _phong_brdf(p_color, p_shininess){};
 
         Vec3f shade(const Ray &p_ray, const HitRecord &p_hitRecord, const LightSample &p_lightSample) const override
         {
             Vec3f diffuse = _lambert_brdf.evaluate();
             Vec3f specular = _phong_brdf.evaluate(p_hitRecord._normal, p_lightSample._direction, -p_ray.getDirection());
-            return diffuse + specular;
+
+            if (_additive)
+                return diffuse + specular * _metalness;
+            else
+                return glm::mix(diffuse, specular, _metalness);
         }
 
         const Vec3f &getFlatColor() const override
@@ -28,7 +36,8 @@ namespace RT_ISICG
         }
 
     private:
-        float _factor;
+        bool _additive = true; // todo explain
+        float _metalness;
         LambertBRDF _lambert_brdf;
         PhongBrdf _phong_brdf;
     };
