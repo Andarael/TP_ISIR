31d0fa440ae6495c3a25e5eaf6bfd5a68651a681
Andarael <josuraad@gmail.com>
2022-04-10 02:03:40 +0200

    fixed simple quad light

--- src/lights/SimpleQuadLight.hpp
+++ src/lights/SimpleQuadLight.hpp
@@ -11,31 +11,38 @@ namespace RT_ISICG
     public:
         SimpleQuadLight() = delete;
 
-        SimpleQuadLight(Vec3f p_color, float power, float p_area, const Vec3f &p_position, const Vec3f &p_normal)
-            : QuadLight(p_color, power, p_position, VEC3F_ZERO, VEC3F_ZERO), _length(glm::sqrt(p_area))
+        SimpleQuadLight(const Vec3f p_color, const float power, const float p_size, const Vec3f &p_position, const Vec3f &p_normal = Vec3f(0, -1, 0))
+            : QuadLight(p_color, power, p_position, VEC3F_ZERO, VEC3F_ZERO),
+              _origin(p_position),
+              _length(p_size)
         {
-            compute_uv(p_area, p_normal);
-            _position = _position + (normalize(_v) - normalize(_u)) * 0.5f * _length;
-            // tood find a better way
+            compute_uv(p_size, p_normal);
+            // todo find a better way
         }
 
         void setLookAt(const Vec3f &lookAt)
         {
-            compute_uv(_length * _length, glm::normalize(lookAt - _position));
+            compute_uv(_length * _length, glm::normalize(lookAt - _origin));
         }
 
     protected:
+        Vec3f _origin; // the center of the square, _position will be used to compute the uv in sample()
         float _length;
 
     private:
-        void compute_uv(const float p_area, const Vec3f &p_normal)
+        void compute_uv(const float p_size, const Vec3f &p_normal)
         {
-            Vec3f vec = Vec3f(0, 1, 1);
+            Vec3f vec = Vec3f(1, 0, 0);
+
+            if (glm::abs(dot(vec, p_normal)) <= float(SHADOW_EPSILON))
+                vec = Vec3f(0, 1, 1);
+
             _u = _length * cross(p_normal, vec);
             _v = _length * normalize(cross(_u, p_normal));
             _u = _length * normalize(cross(_v, p_normal));
-            _area = p_area;
+            _area = p_size * p_size;
             _normal = normalize(p_normal);
+            _position = _origin + (normalize(_v) - normalize(_u)) * 0.5f * _length;
         }
     };
 
