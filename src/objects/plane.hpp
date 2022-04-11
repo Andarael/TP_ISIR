c8251fc26b44e5f73eabec9a1ad8a1518b7675e9
Andarael <josuraad@gmail.com>
2022-04-11 05:25:16 +0200

    simpler intersection range check

--- src/objects/plane.hpp
+++ src/objects/plane.hpp
@@ -19,7 +19,7 @@ namespace RT_ISICG
             float t;
             if (_geometry.intersect(p_ray, t, t))
             {
-                if (!intersectionInRange(t, t, p_tMin, p_tMax))
+                if (!intersectionInRange(t, p_tMin, p_tMax))
                     return false;
 
                 Vec3f compute_normal = _geometry.computeNormal(p_ray.pointAtT(t));
@@ -33,9 +33,9 @@ namespace RT_ISICG
 
         bool intersectAny(const Ray &p_ray, const float p_tMin, const float p_tMax) const override
         {
-            float t1, t2;
-            if (_geometry.intersect(p_ray, t1, t2))
-                return intersectionInRange(t1, t2, p_tMin, p_tMax);
+            float t;
+            if (_geometry.intersect(p_ray, t, t))
+                return intersectionInRange(t, p_tMin, p_tMax);
             return false;
         }
 
