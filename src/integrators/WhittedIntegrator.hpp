60cd09188414290f438e07c439e35339dbc22efc
Andarael <josuraad@gmail.com>
2022-04-11 05:33:10 +0200

    working Fresnel

--- src/integrators/WhittedIntegrator.hpp
+++ src/integrators/WhittedIntegrator.hpp
@@ -24,7 +24,7 @@ namespace RT_ISICG
         }
 
     protected:
-        Vec3f trace(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth = 0, const float currentIOR = 1.0f) const
+        Vec3f trace(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth = 0) const
         {
             if (depth > _nbBounces)
                 return BLACK;
@@ -33,33 +33,35 @@ namespace RT_ISICG
             if (p_scene.intersect(p_ray, p_tMin, p_tMax, hitRecord))
             {
                 if (hitRecord._object->getMaterial()->isMirror())
-                    return reflectRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord, 1.0f);
+                    return reflectRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord);
 
                 if (hitRecord._object->getMaterial()->isTransparent())
-                    return reflectRefractRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord, currentIOR);
+                    return reflectRefractRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord);
 
                 return directLighting(p_scene, p_ray, hitRecord);
             }
             return _backgroundColor;
         }
 
+        // compute only the reflectance
         float fresnel(const Vec3f &wi, const Vec3f &N, float n1, float n2) const
         {
             float cosI = glm::dot(wi, N);
-            Vec3f n = N;
-            /*     if (cosI > 0.0f)
-                 {
-                     float tmp = n1;
-                     n1 = n2;
-                     n2 = tmp;
-                 }*/
 
-            float sinT = n1 / n2 * (1.0f - cosI * cosI);
+            if (cosI > 0.0f)
+            {
+                float tmp = n1;
+                n1 = n2;
+                n2 = tmp;
+            }
+
+            float n = n1 / n2;
+            float sinT = n * n * (1.0f - cosI * cosI);
 
             if (sinT > 1.0f)
-                return 1.0f;
+                return 1.0f; // no refraction
 
-            float cosT = glm::sqrt(1.0f - sinT * sinT);
+            float cosT = glm::sqrt(glm::max(0.0f, 1.0f - sinT * sinT));
             cosI = glm::abs(cosI);
 
             float Rs = ((n2 * cosI) - (n1 * cosT)) / ((n2 * cosI) + (n1 * cosT));
@@ -70,44 +72,46 @@ namespace RT_ISICG
 
         Vec3f refractRay(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth, const HitRecord &hitRecord, const float n1, const float n2, const float fresnelFactor) const
         {
-            if (fresnelFactor > 1.f)
-                return Vec3f(1, 0, 1);
+            if (fresnelFactor >= 1.f)
+                return BLACK;
 
-            Vec3f wi = -p_ray.getDirection();
+            float r = n1 / n2;
+            Vec3f normal = hitRecord._backFacing ? -hitRecord._normal : hitRecord._normal;
 
-            bool isInside = glm::dot(wi, hitRecord._normal) < 0.0f;
-
-            Vec3f normal = isInside ? -hitRecord._normal : hitRecord._normal;
-            float r = isInside ? n1 / n2 : n2 / n1;
-
-            Vec3f refractedDirection = glm::refract(-wi, normal, r);
+            Vec3f refractedDirection = glm::refract(p_ray.getDirection(), normal, r);
             Ray refractedRay = Ray(hitRecord._point, refractedDirection);
 
-            refractedRay.offset(-normal);
+            refractedRay.offset(-normal); // we always reflect in a different medium than the ray origin
 
             Vec3f transparentColor = hitRecord._object->getMaterial()->getFlatColor();
-            // return refractedDirection;
-            return glm::max(VEC3F_ZERO, transparentColor * trace(p_scene, refractedRay, p_tMin, p_tMax, depth + 1, n2)); // todo why n2² ?
+            return transparentColor * trace(p_scene, refractedRay, p_tMin, p_tMax, depth + 1);
         }
 
-        Vec3f reflectRay(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth, const HitRecord &hitRecord, const float ior) const
+        Vec3f reflectRay(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth, const HitRecord &hitRecord) const
         {
-            Ray reflectedRay = Ray(hitRecord._point, glm::reflect(p_ray.getDirection(), hitRecord._normal));
-            reflectedRay.offset(hitRecord._normal);
+            Vec3f normal = hitRecord._backFacing ? -hitRecord._normal : hitRecord._normal;
+            Ray reflectedRay = Ray(hitRecord._point, glm::reflect(p_ray.getDirection(), normal));
+            reflectedRay.offset(normal); // we always reflect in the same medium as the ray origin
+
             Vec3f mirrorColor = hitRecord._object->getMaterial()->getFlatColor();
-            return mirrorColor * trace(p_scene, reflectedRay, p_tMin, p_tMax, depth + 1, ior);
+            return mirrorColor * trace(p_scene, reflectedRay, p_tMin, p_tMax, depth + 1);
         }
 
-        Vec3f reflectRefractRay(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth, const HitRecord &hitRecord, const float currentIOR) const
+        Vec3f reflectRefractRay(const Scene &p_scene, const Ray &p_ray, const float p_tMin, const float p_tMax, const int depth, const HitRecord &hitRecord) const
         {
-            float n1 = currentIOR;
+            float n1 = 1.0f;
             float n2 = hitRecord._object->getMaterial()->getIOR();
 
-            float fresnelFactor = fresnel(-p_ray.getDirection(), hitRecord._normal, n1, n2); // todo use wi = -ray.direction
-            Vec3f reflectedColor = reflectRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord, n1);
-            Vec3f refractedColor = refractRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord, n1, n2, fresnelFactor);
+            float fresnelFactor = fresnel(p_ray.getDirection(), hitRecord._normal, n1, n2);
 
-            // return Vec3f(fresnelFactor);
+            if (hitRecord._backFacing)
+            {
+                n1 = n2;
+                n2 = 1.0f;
+            }
+
+            Vec3f reflectedColor = reflectRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord);
+            Vec3f refractedColor = refractRay(p_scene, p_ray, p_tMin, p_tMax, depth, hitRecord, n1, n2, fresnelFactor);
 
             return glm::mix(refractedColor, reflectedColor, fresnelFactor);
         }
