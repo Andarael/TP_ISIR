e928770eec820630287480dd2b0bde5933e0f73a
Andarael <josuraad@gmail.com>
2022-05-14 15:28:35 +0200

    removed old uchar pixels, added todo

--- src/texture.hpp
+++ src/texture.hpp
@@ -4,9 +4,9 @@
 #define __RT_ISICG_IMAGE__
 
 #include "defines.hpp"
+#include <stb/stb_image.h>
 #include <string>
 #include <vector>
-#include <stb/stb_image.h>
 
 namespace RT_ISICG
 {
@@ -18,10 +18,8 @@ namespace RT_ISICG
         Texture(const int p_width, const int p_height, const int _nbChannels = 3)
             : _nbChannels(_nbChannels),
               _width(p_width), _height(p_height),
-              // _pixels(_width * _height * _nbChannels, 0),
               _pixelsFloat(_width * _height * _nbChannels, 0.f)
         {
-            // _pixels.shrink_to_fit();
             _pixelsFloat.shrink_to_fit();
         }
 
@@ -32,10 +30,7 @@ namespace RT_ISICG
             if (data)
             {
                 for (int i = 0; i < _width * _height * _nbChannels; i++)
-                {
-                    // _pixels[i] = data[i];
                     _pixelsFloat[i] = float(data[i]) / 255.f;
-                }
                 stbi_image_free(data);
             }
             else
@@ -57,6 +52,7 @@ namespace RT_ISICG
 
         Vec3f &getPixel(const float p_x, const int p_y) const
         {
+            // todo: implement bilinear interpolation
         }
 
         int getWidth() const { return _width; }
@@ -79,7 +75,7 @@ namespace RT_ISICG
         int _nbChannels = 3;
         int _width;
         int _height;
-        // std::vector<unsigned char> _pixels;
+
         std::vector<float> _pixelsFloat;
     };
 } // namespace RT_ISICG
